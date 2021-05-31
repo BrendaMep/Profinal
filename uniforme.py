f17a74416ebed8f9d15aba8b57e9d84b227f4bca
lalo <zs17013388@estudiantes.uv.mx>
2021-05-31 11:22:27 -0500

    Se modifico los espaciados para seguir los estandares del pep8 en el archivos BusquedasProfundidad.py
    Creo haber terminado mi algoritmo de busqueda costo uniforme, sin emabrgo aun debo revisar si los vecinos en mi mapa estan ordenados por menor costo de viaje.

--- uniforme.py
+++ uniforme.py
@@ -1,3 +1,63 @@
-from BusquedasProfundidad import mapa
+A = 'Acayucan'
+B = 'Boca del Rio'
+C = 'Coatzacualcos'
+D = 'Agua Dulce'
+E = 'Huautla de Jimenez'
+F = 'Fortin Flores'
+G = 'Vega de Alatorre'
+H = 'Huatusco'
+J = 'Joachin'
+M = 'Minatitlan'
+N = 'Nigromante'
+O = 'Otatitlan'
+P = 'Papantla'
+S = 'San Andres Tuxtla'
+T = 'Tecolutla'
+U = 'Teziutlan'
+V = 'Alvarado'
+X = 'Xalapa'
+Y = 'Yanga'
+Z = 'Zempoala'
 
-print(mapa)
+mapa = {
+    'A': ['Z', 'T', 'S'],
+    'B': ['F', 'P', 'G', 'U'],
+    'C': ['D', 'P', 'R'],
+    'D': ['M', 'C'],
+    'E': ['H'],
+    'F': ['S', 'B'],
+    'G': ['B'],
+    'H': ['U', 'E'],
+    'I': ['N', 'V'],
+    'L': ['T', 'M'],
+    'M': ['L', 'D'],
+    'N': ['I'],
+    'O': ['Z', 'S'],
+    'P': ['R', 'C', 'B'],
+    'R': ['S', 'C', 'P'],
+    'S': ['A', 'O', 'F', 'R'],
+    'T': ['A', 'L'],
+    'U': ['B', 'V', 'H'],
+    'V': ['U', 'I'],
+    'Z': ['O', 'A']
+}
+
+
+def costo(mapa, inicio, final):
+    ruta = []
+    while inicio != final:
+        vecinos = mapa[inicio]
+        ruta.append(inicio)
+        vecinos2 = list()
+        if final in vecinos:
+            inicio = final
+            ruta.append(final)
+            return ruta
+        else:
+            for ciudad in vecinos:
+                if ciudad not in ruta:
+                    vecinos2.append(ciudad)
+            inicio = vecinos2[0]
+
+
+
