db27ab341384b98965d76e466a50fb56a13e9ec9
Home <BrendaMep>
2021-05-31 11:57:44 -0500

    Se tenia algunos problemas con la funcion de ruta pero ahora si funciona como debe ser

--- BusquedasProfundidad.py
+++ BusquedasProfundidad.py
@@ -24,41 +24,43 @@ ciudades = [A, B, C, D, E, F, G, H, I, L, M, N, O, P, R, S, T, U, V, Z]
 mapa = [[Z, T, S], [F, P, G, U], [D, P, R], [M, C], [H], [S, B], [B], [U, E], [N, V], [T, M], [L, D],
         [I], [Z, S], [R, C, B], [S, C, P], [A, O, F, R], [A, L], [B, V, H], [U, I], [O, A]]
 
+
 def ruta(recor):
     x = len(recor)
     rut = []
     rut.append(recor[x-1][0])
     rut.append(recor[x-1][1])
-    for j in range(len(rut)):
+    m = len(rut)
+    while m < x:
         for i in range(x):
+            j = len(rut) - 1
             if rut[j] == recor[i][0]:
-                rut.append(recor[i][1])
-    rut = list(set(rut))
-    rut.reverse()
+                if recor[i][1] is not rut:
+                    rut.append(recor[i][1])
+                    break
+        m = m+1
+    return rut
 
 
 def profundidad(mapa, inicio, destino):
     arbol = list()
+    recorrido = list()
     arbol.append(inicio)
-    recorrido = [[inicio, None]]
-    while len(arbol) != 0:
-        x = len(arbol)
+    recorrido.append([inicio, None])
+    x = len(arbol)
+    while x != 0:
         ciudad = arbol[x-1]
         arbol.remove(ciudad)
         if ciudad == destino:
-            return ruta(recorrido)
-        for i in range(20):
-            if inicio == ciudades[i]:
-                for j in mapa[i]:
-                    if j not in recorrido:
-                        arbol.append(j)
-                        recorrido.append([j, ciudad])
-                break
-        print(arbol)
-        print(recorrido)
-        print(ciudad)
-        print(len(recorrido))
-        #arbol = arbol
-        break
+            print(ruta(recorrido))
+            return
+        else:
+            vecinos = mapa[ciudades.index(ciudad)]
+            for vecino in vecinos:
+                if vecino is not recorrido:
+                    arbol.append(vecino)
+                    recorrido.append([vecino,ciudad])
+        x = x+1
+
 
 print(profundidad(mapa,A,B))
