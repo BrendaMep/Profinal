5e000ec279cf42a57a4943a5502cccfe98b41599
Home <BrendaMep>
2021-05-31 14:48:02 -0500

    Busqueda por profundidad esta terminada.

--- BusquedasProfundidad.py
+++ BusquedasProfundidad.py
@@ -22,10 +22,9 @@ Z = 'Zempoala'
 
 # ciudades vecinas de cada una de las ciudades
 ciudades= [A, B, C, D, E, F, G, H, J, M, N, O, P, S, T, U, V, X, Y, Z]
-mapa_ej = [[M, S, N], [Z, V, J, X], [M, D, S], [C], [O, F], [Y, H, E], [T, P, Z, X], [F, X],
+mapa = [[M, S, N], [Z, V, J, X], [M, D, S], [C], [O, F], [Y, H, E], [T, P, Z, X], [F, X],
            [Y, B, O], [C, A], [O, A], [J, N, V, E], [T, G, U], [V, A, C], [G, P], [X, P],
            [B, S, O], [Z, U, B, H, G], [F, J], [B, X, G]]
-#print(mapa_ej[7])
 
 def ruta(recor):
     x = len(recor)
@@ -52,21 +51,23 @@ def profundidad(mapa, inicio, destino):
     recorrido.append([inicio, None])
     while len(arbol) != 0:
         ciudad = arbol[len(arbol) - 1]
-        #print(ciudad)
-        #print(arbol)
         arbol.remove(arbol[len(arbol)-1])
         if ciudad == destino:
-            #print(recorrido)
             ru = ruta(recorrido)
             return ru
         else:
             vecinos = mapa[ciudades.index(ciudad)]
             for vecino in vecinos:
-                acceso = 0
-                for ndo in recorrido:
-                    if (vecino == ndo[0] or vecino == ndo[1]):
-                        acceso += 1
-                if acceso == 0:
+                if vecino == destino:
                     arbol.append(vecino)
                     recorrido.append([vecino, ciudad])
-print(profundidad(mapa_ej,A,B))
+                    break
+                else:
+                    acceso = 0
+                    for ndo in recorrido:
+                        if (vecino == ndo[0]) or (vecino == ndo[1]):
+                            acceso += 1
+                    if acceso == 0:
+                        arbol.append(vecino)
+                        recorrido.append([vecino, ciudad])
+#print(profundidad(mapa,A,X))
--- mapa1.py
+++ mapa1.py
@@ -25,12 +25,3 @@ ciudades = [A,B,C,D,E,F,G,H,J,M,N,O,P,S,T,U,V,X,Y,Z]
 mapa = [[M, S, N], [Z, V, J, X], [M, D, S], [C], [O, F], [Y, H, E], [T, P, Z, X], [F, X],
         [Y, B, O], [C, A], [O,A], [J, N, V, E], [T, G, U], [V, A, C], [G, P], [X, P],
         [B, S, O], [Z, U, B, H, G], [F, J], [B, X, G]]
-
-print(len(mapa))
-
-
-#Aqui se obtiene cada ciudad vecina de una en especifico.
-
-for i in range(20):
-    for j in range(len(mapa[i])):
-        print(ciudades[i] ,mapa[i][j])
