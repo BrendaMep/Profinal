e940569b6396d992aa89177a3ca7d2f6b6bbd207
Home <BrendaMep>
2021-06-04 17:58:22 -0500

    se le quitaron los for y se agrego uno  para que agregue mejor los elementos

--- Ventana.py
+++ Ventana.py
@@ -20,10 +20,15 @@ class ProjectWindow(QMainWindow):
         self.lyt_settings = QVBoxLayout()
         self.lyt_graph = QHBoxLayout()
 
-
+        self.mensaje = QFormLayout()
         self.lyt_salida = QFormLayout()
         self.lyt_destino = QFormLayout()
+        self.descripcion1 = QFormLayout()
+        self.descripcion2 = QFormLayout()
+        self.descripcion3 = QFormLayout()
         self.lyt_modo = QFormLayout()
+
+        self.mensaje = QLabel()
 
         self.lbl_salida = QLabel()
         self.sld_salida = QComboBox()
@@ -33,35 +38,39 @@ class ProjectWindow(QMainWindow):
         self.sld_destino = QComboBox()
         #self.lnedt_rho = QLineEdit()
 
+        self.descripcion1 = QLabel()
+        self.descripcion2 = QLabel()
+        self.descripcion3 = QLabel()
+
         self.lbl_modo = QLabel()
         self.sld_modo = QComboBox()
         #self.lnedt_beta = QLineEdit()
 
         self.btn_graph = QPushButton()
 
+        # graficas
+
         self.setup_ui()
 
     def setup_ui(self):
         self.setWindowTitle('Viaje')
-        mensaje = QLabel("Bienvenido")
+        self.mensaje = QLabel("Bienvenido")
 
         self.lbl_salida.setText('Salida')
         self.lbl_salida.setFixedWidth(50)
-        for i in lista:
-            self.sld_salida.addItem(i)
+        self.sld_salida.addItems(lista)
         self.lbl_salida.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
         self.sld_salida.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
 
         self.lbl_destino.setText('Destino')
         self.lbl_destino.setFixedWidth(65)
-        for i in lista:
-            self.sld_destino.addItem(i)
+        self.sld_destino.addItems(lista)
         self.lbl_destino.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
         self.sld_destino.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
 
-        descripcion1 = QLabel("Rapida, es la ruta mas corta de llegar al destino.")
-        descripcion2 = QLabel("Bajo, es la ruta con menos costo.")
-        descripcion3 = QLabel("Larga, es la ruta donde viajaras por mas ciudades.")
+        self.descripcion1 = QLabel("Rapida, es la ruta mas corta de llegar al destino.")
+        self.descripcion2 = QLabel("Bajo, es la ruta con menos costo.")
+        self.descripcion3 = QLabel("Larga, es la ruta donde viajaras por mas ciudades.")
 
 
         #mapa de html
@@ -74,21 +83,18 @@ class ProjectWindow(QMainWindow):
 
         self.lbl_modo.setText('Modo de viaje')
         self.lbl_modo.setFixedWidth(110)
-        for i in lista2:
-            self.sld_modo.addItem(i)
+        self.sld_modo.addItems(lista2)
         self.lbl_modo.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
         self.sld_modo.setSizePolicy(QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed))
 
         self.btn_graph.setText('Trazar ruta')
         self.btn_graph.setFixedWidth(105)
 
-        #viaje = QLabel(" 1111")
-
         self.main_layout.addLayout(self.lyt_settings) # objetos que conforman la interfaz
         self.main_layout.addLayout(self.lyt_graph)
         self.container.setLayout(self.main_layout)
 
-        self.lyt_settings.addWidget(mensaje)
+        self.lyt_settings.addWidget(self.mensaje)
 
         self.lyt_settings.addWidget(self.lbl_salida)
         self.lyt_salida.addRow(self.sld_salida)  #, self.lnedt_sigma)
@@ -98,9 +104,9 @@ class ProjectWindow(QMainWindow):
         self.lyt_destino.addRow(self.sld_destino)   #, self.lnedt_rho)
         self.lyt_settings.addLayout(self.lyt_destino)
 
-        self.lyt_settings.addWidget(descripcion1)
-        self.lyt_settings.addWidget(descripcion2)
-        self.lyt_settings.addWidget(descripcion3)
+        self.lyt_settings.addWidget(self.descripcion1)
+        self.lyt_settings.addWidget(self.descripcion2)
+        self.lyt_settings.addWidget(self.descripcion3)
 
         self.lyt_settings.addWidget(self.lbl_modo)
         self.lyt_modo.addRow(self.sld_modo)    #, self.lnedt_beta)
@@ -108,9 +114,18 @@ class ProjectWindow(QMainWindow):
 
         self.lyt_settings.addWidget(self.btn_graph)
 
-        #self.lyt_settings.addWidget(viaje)
+
+
+
+
+
+
+
+
 
         self.setCentralWidget(self.container)
+
+
 
 
 
